05265c1b55ecd1b392fe1a248c1a6174acaf9404
pippo0076003 <143545753+pippo0076003@users.noreply.github.com>
2023-10-09 18:41:57 +0200

    Delete plugins directory

--- plugins/prueba-idioma-dinamico.js
+++
@@ -1,6 +0,0 @@
-const handler = async (m, {text, command, args, usedPrefix}) => {
-let language = 'en'
-m.reply(`${lenguajeGB['smsAvisoAG']()}`)
-} 
-handler.command = /^(prueba39)$/i
-export default handler
